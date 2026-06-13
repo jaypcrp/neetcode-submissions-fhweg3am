c25e66a3792f84915a50a1a5017f198a0fef2969
jaypcrp <jayprajapaticrp@gmail.com>
2026-06-13 13:14:09 +0530

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ // Optimal solution: 
+ // Think of it like: If the road is straight (i.e Straight Linked list ending at null) , two riders with different speed (constant) never meets.
+ //                   But if the road is ciruclar (i.e cyclic , end is connected to somewhere in road), two riders with different speed (constant), will definetely meet.
+
+ // This is called as Tortoise and Hare Algorithm
+ // 
+ // Time Complexity: O(n)
+ // Space Complexity: O(1)
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
